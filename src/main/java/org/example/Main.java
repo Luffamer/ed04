d7ddcf346ef61962a35676294630b48ff898fce6
Luffamer <1804564@alu.murciaeduca.es>
2024-06-05 22:27:46 +0200

    Creacion de initAgenda

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,9 +6,7 @@ public class Main {
         agendaContactos agendaContactos = new agendaContactos();
 
         // Añadir contactos a la agenda.
-        agendaContactos.addContact("John Doe", "1234567890");
-        agendaContactos.addContact("Jane Doe", "9876543210");
-        agendaContactos.addContact("Mary Jane", "1122334455");
+        initAgenda(agendaContactos);
 
         // Imprimir todos los contactos de la agenda.
         System.out.println("Todos los contactos: ");
@@ -29,5 +27,11 @@ public class Main {
         for (Contacto c : agendaContactos.getContacts()) {
             System.out.println(c.getName() + " -> " + c.getPhones());
         }
+    }
+
+    private static void initAgenda(agendaContactos agendaContactos) {
+        agendaContactos.addContact("John Doe", "1234567890");
+        agendaContactos.addContact("Jane Doe", "9876543210");
+        agendaContactos.addContact("Mary Jane", "1122334455");
     }
 }
